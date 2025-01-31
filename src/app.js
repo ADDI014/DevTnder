93bfb04b3c3fb04f4c3de22a3434bf16ffedf9f4
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-01-31 22:50:28 +0530

    Exploring get Post delete

--- src/app.js
+++ src/app.js
@@ -2,19 +2,44 @@ const express = require("express");
 
 const app = express();
 
-app.use('/',(req,res) => {
-    res.send("Hello From the DashBoard");
+
+// app.use('/',(req,res) => {
+//     res.send("Hello From the DashBoard");
+// });
+
+// app.use("/test", (req,res) => {
+//     res.send("Hello From the server");
+// });
+
+// app.use("/hello/2",(req,res) => {
+//     res.send("Abracadabra");
+// });
+
+// app.use("/hello",(req,res) => {
+//     res.send("Hello Hello Hello");
+// });
+
+app.use("/user",(req,res)=> {
+    res.send("hahahahahahha");
+})
+
+app.get("/user", (req , res) => {
+    res.send({firstName : "Ankit" , lastName: "Kumar"});
+})
+
+app.post("/user", (req,res) => {
+    res.send("Data successfully saved to the database!");
+})
+
+app.delete("/user",(req,res) => {
+    res.send("deleted successfully");
 });
 
-app.use("/hello",(req,res) => {
-    res.send("Hello Hello Hello");
-});
 
-app.use("/test", (req,res) => {
-    res.send("Hello From the server");
-});
+
 
 app.listen(7777, () => {
     console.log("Server is listening on port 7777...");
 })
+
 
