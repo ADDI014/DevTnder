b856b4313dbe1c6aff6d2460f560ee8e3c32c0a1
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-02-04 21:35:52 +0530

    middleswares

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const {adminAuth , UserAuth} = require("./middlewares/auth");
+
 
 // app.use('/',(req,res) => {
 //     res.send("Hello From the DashBoard");
@@ -55,17 +57,43 @@ const app = express();
 //      res.send("Response 4");
 //  }]);
 
-app.get("/user",(req,res, next) => {
-    console.log("Handling the route user1!!");
+// app.get("/user",(req,res, next) => {
+//     console.log("Handling the route user1!!");
     // res.send("Response");
-    next();
+//     next();
+// });
+
+// app.get("/user",(req,res, next) => {
+//     console.log("Handling the route user2!!");
+    // res.send("Response2");
+//     next();
+// });
+
+
+
+
+//handle Auth Middleware for all GET POST ,... requests
+app.use('/admin', adminAuth );
+
+app.get("/user", (req,res) => {
+    res.send("Uaser data Sent");
 });
 
-app.get("/user",(req,res, next) => {
-    console.log("Handling the route user2!!");
-    // res.send("Response2");
-    next();
+app.get("/admin/getAllData",(req,res)=> {
+    res.send("All data sent");
+})
+
+app.get("/user/login", (req,res)=>{
+    res.send("user logged in successfully");
+})
+
+app.get("/user", UserAuth, (req,res) => {
+    res.send("Uaser data Sent");
 });
+
+app.get("/admin/deleteUser",(req,res)=> {
+    res.send("Deleted a User");
+})
 
 
 
