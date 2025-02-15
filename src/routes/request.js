0a525734dd1fffb79a494a8af1e76891d03061c3
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-02-15 21:43:42 +0530

    checking to user is exists or not

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,7 +2,7 @@ const express = require('express')
 const requestRouter = express.Router()
 const {UserAuth}= require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-
+const User = require("../models/user");
 
 requestRouter.post("/request/send/:status/:toUserId" , UserAuth, async (req,res) => {
    try{
@@ -15,6 +15,12 @@ requestRouter.post("/request/send/:status/:toUserId" , UserAuth, async (req,res)
     if(!allowedStatus.includes(status)){
         return res.status(400).json({message : "Invalid status type : " + status});
     }
+
+    const toUser = await User.findById(toUserId);
+    if(!toUser){
+        return res.status(404).json({message : "User not found"});
+    }
+
 
     const existingConnectionRequest = await ConnectionRequest.findOne({
         $or : [
