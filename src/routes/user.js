c27e8b966ebafa3bbe2e63ed1f5eda9177b4d04b
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-12 15:05:59 +0530

    getting all the connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,12 @@ const ConnectionRequestModel = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
+
+
+//ye sabhi pending interested requests ki list dega
 userRouter.get("/user/requests/received",UserAuth , async (req,res) => {
     try{
         const loggedInUser = req.user;
@@ -11,7 +17,7 @@ userRouter.get("/user/requests/received",UserAuth , async (req,res) => {
         const connectionRequest = await ConnectionRequestModel.find({
             toUserId : loggedInUser._id,
             status : "interested",
-        }).populate("fromUserId" , "firstName lastName photoUrl age about skills");
+        }).populate("fromUserId" , USER_SAFE_DATA);
         
         // .populate("fromUserId",["firstName" , "lastName"]);
 
@@ -24,5 +30,33 @@ userRouter.get("/user/requests/received",UserAuth , async (req,res) => {
         req.statusCode(400).send("ERROR" + err.message);
     }
 })
+
+userRouter.get("/user/connections", UserAuth, async (req ,res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or : [
+                {toUserId : loggedInUser._id, status : "accepted"},
+                {fromUserId: loggedInUser._id, status :"accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+       const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+    }
+    catch(err) {
+        res.status(400).send({message : err.message});
+    }
+})
+
+
 
 module.exports = userRouter;
