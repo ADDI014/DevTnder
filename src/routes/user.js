3a7452d69d7c8efb205d3e7b503c1ec34a39f99a
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-17 23:23:05 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -64,6 +64,12 @@ userRouter.get("/user/feed", UserAuth, async (req,res) => {
 
         const loggedInUser = req.user;
 
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1) * limit;
+
         const connectionRequests = await ConnectionRequestModel.find({
         $or : [{fromUserId : loggedInUser._id}, {toUserId : loggedInUser._id}],  
         }).select("fromUserId toUserId");
@@ -81,7 +87,7 @@ userRouter.get("/user/feed", UserAuth, async (req,res) => {
                 {_id  : { $nin : Array.from(hideUsersFromFeed)}},
                 {_id : {$ne : loggedInUser._id}},
             ],
-        }).select(USER_SAFE_DATA);
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
         res.send(users);
     }
