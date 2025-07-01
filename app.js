ccf880db09a97043c4af169fe890092254ab376b
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-07-01 16:22:45 +0530

    add port into env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 7777;
 
 const connectDB = require("./src/config/database");
 
@@ -63,7 +64,7 @@ app.patch("/user/:userId", async (req , res) => {
 connectDB().then(() => {
     console.log("Database connection established");
 
-    app.listen(7777, () => {
+    app.listen(PORT, () => {
         console.log("Server is listening on port 7777...");
     })
 }).catch((err) => {
