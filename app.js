d1148bae5de333b65102410d963fbbb29d5ccbbc
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-07-01 16:13:36 +0530

    add frontend url

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 const connectDB = require("./src/config/database");
 
 app.use(cors(
-    { origin: 'http://localhost:5173',
+    { origin: ['http://localhost:5173',
+        "https://devsang.netlify.app",
+    ],
         credentials : true
      }
 ));
