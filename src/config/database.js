c0d93cc86c91b81ce427c46df28105a25e8a48da
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-07-02 21:43:24 +0530

    added new connnection url

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://ankit2237129aiml:Ankit132asdf@alok.iadhh.mongodb.net/devTinder"
+        // "mongodb+srv://ankit2237129aiml:Ankit132asdf@alok.iadhh.mongodb.net/devTinder"
+        'mongodb+srv://ADDI0147C:TK6ngsa2CuTpQiWo@cluster0.mdtnub5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     );
 };
 
