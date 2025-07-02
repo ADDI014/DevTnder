25ab45af65c61ca754bb9a44f316e231f8119daa
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-07-02 22:38:01 +0530

    add mongo uri in .env

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(
-        // "mongodb+srv://ankit2237129aiml:Ankit132asdf@alok.iadhh.mongodb.net/devTinder"
-        'mongodb+srv://ADDI0147C:TK6ngsa2CuTpQiWo@cluster0.mdtnub5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        process.env.MONGODB_URI
     );
 };
 
